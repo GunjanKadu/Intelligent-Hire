4133074e341a96b183e567d97924c551496ffdc2
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 09:04:49 +0100

    Footer Styles

---
+++ code/web/src/modules/common/Footer/styles.js
@@ -0,0 +1,28 @@
+// UI Imports
+import grey from '@material-ui/core/colors/grey'
+
+// Component Styles
+const styles = {
+  root: {
+    backgroundColor: grey[50],
+    padding: '0.5rem 1rem',
+    color: grey[500],
+    borderTop: `1px solid ${ grey[200] }`
+  },
+  left: {
+    textAlign: 'left'
+  },
+  right: {
+    textAlign: 'right'
+  },
+  link: {
+    color: 'inherit',
+    display: 'inline',
+    marginLeft: '1rem'
+  },
+  copyright: {
+    color: grey[500]
+  }
+}
+
+export default styles
