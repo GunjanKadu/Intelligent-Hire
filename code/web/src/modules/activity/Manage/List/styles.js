d2dc42aaafdfd20964263443b18c3e35d42a810f
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 08:33:32 +0100

    Styles for activity list

---
+++ code/web/src/modules/activity/Manage/List/styles.js
@@ -0,0 +1,21 @@
+// Component Styles
+const styles = theme => ({
+  root: {
+    maxHeight: 'calc(100vh - 200px)',
+    overflowY: 'auto'
+  },
+
+  textCenter: {
+    textAlign: 'center'
+  },
+
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, 0.15)'
+  },
+
+  drawer: {
+    width: 450
+  }
+})
+
+export default styles
