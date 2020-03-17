f7ffb87e6e7e3a38b1d67e65d6efec10704f054d
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 11:05:30 +0100

    User account verify styles

---
+++ code/web/src/modules/user/Verify/styles.js
@@ -0,0 +1,16 @@
+// UI Imports
+import grey from '@material-ui/core/colors/grey'
+
+// Component Styles
+const styles = theme => ({
+  buttonsContainer: {
+    textAlign: 'right',
+    marginTop: theme.spacing()
+  },
+
+  panel: {
+    backgroundColor: `${ grey[50] } !important`,
+  }
+})
+
+export default styles
