5314b3adc0ccd93ae4e8416ea803446b65b6c508
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 11:03:05 +0100

    User Access login sttyles

---
+++ code/web/src/modules/user/Access/Login/styles.js
@@ -0,0 +1,17 @@
+// Component Styles
+const styles = theme => ({
+  root: {
+    padding: theme.spacing()
+  },
+
+  buttonsContainer: {
+    textAlign: 'right',
+    marginTop: theme.spacing()
+  },
+
+  heading: {
+    marginBottom: theme.spacing(2)
+  }
+})
+
+export default styles
