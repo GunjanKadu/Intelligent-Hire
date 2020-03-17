a4dd37e9acf3205392c5b94c7e1b8b09e12e2150
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 11:13:15 +0100

    Dashboard routes

---
+++ code/web/src/setup/routes/dashboard.js
@@ -0,0 +1,11 @@
+// App Imports
+import Dashboard from '../../modules/dashboard'
+
+// Pages routes
+export default {
+  dashboard: {
+    path: '/dashboard',
+    component: Dashboard,
+    auth: true
+  }
+}
