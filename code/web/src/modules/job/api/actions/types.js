e67ab3a3585c66b029dcf0410b52d841d2d9777c
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 10:51:17 +0100

    Job manage cache and action types

---
+++ code/web/src/modules/job/api/actions/types.js
@@ -0,0 +1,22 @@
+// Actions Types
+
+// Single
+export const SINGLE_REQUEST = 'JOB/SINGLE/REQUEST'
+export const SINGLE_RESPONSE = 'JOB/SINGLE/RESPONSE'
+export const SINGLE_DONE = 'JOB/SINGLE/DONE'
+export const SINGLE_RESET = 'JOB/SINGLE/RESET'
+
+// List by project
+export const LIST_BY_PROJECT_REQUEST = 'JOB/LIST/BY_PROJECT/REQUEST'
+export const LIST_BY_PROJECT_RESPONSE = 'JOB/LIST/BY_PROJECT/RESPONSE'
+export const LIST_BY_PROJECT_DONE = 'JOB/LIST/BY_PROJECT/DONE'
+export const LIST_BY_PROJECT_RESET = 'JOB/LIST/BY_PROJECT/RESET'
+
+// Edit
+export const EDIT_SET = 'JOB/EDIT/SET'
+export const EDIT_UNSET = 'JOB/EDIT/UNSET'
+
+// View
+export const VIEW_SET = 'JOB/VIEW/SET'
+export const VIEW_UNSET = 'JOB/VIEW/UNSET'
+export const VIEW_HIDE = 'JOB/VIEW/HIDE'
