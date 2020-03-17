0b340dfdd5a4287d64f9a3632a341f42de64aee3
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 10:48:01 +0100

    Invite accect component styles

---
+++ code/web/src/modules/invite/Accept/styles.js
@@ -0,0 +1,20 @@
+// Component Styles
+const styles = theme => ({
+  root: {
+    paddingTop: '10vh',
+    paddingBottom: '10vh'
+  },
+
+  container: {
+    width: '30vw',
+    margin: '0 auto',
+    padding: theme.spacing(2),
+  },
+
+  buttonsContainer: {
+    textAlign: 'right',
+    marginTop: theme.spacing()
+  },
+})
+
+export default styles
