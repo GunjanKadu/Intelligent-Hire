b39f3d87c53b5182b2693b37856c889ab9ac411a
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 10:58:59 +0100

    Project manage list  styles

---
+++ code/web/src/modules/project/Manage/List/styles.js
@@ -0,0 +1,13 @@
+// Component Styles
+const styles = theme => ({
+  root: {
+    maxHeight: 'calc(100vh - 200px)',
+    overflowY: 'auto'
+  },
+
+  textCenter: {
+    textAlign: 'center'
+  }
+})
+
+export default styles
