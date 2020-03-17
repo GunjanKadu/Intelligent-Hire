4cb47d9312ddad98d6f4e93b67b4c1c614197049
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 08:41:19 +0100

    Candidate View component styles

---
+++ code/web/src/modules/candidate/Manage/View/styles.js
@@ -0,0 +1,13 @@
+// Component Styles
+const styles = theme => ({
+  root: {
+    padding: theme.spacing(2),
+    minWidth: 350
+  },
+
+  title: {
+    marginBottom: theme.spacing(3),
+  }
+})
+
+export default styles
