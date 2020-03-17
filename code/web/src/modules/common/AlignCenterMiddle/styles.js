63598264ef2cb1e1f2a6d4bd9383203cfab88b73
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 08:46:45 +0100

    Align Center Middle styles

---
+++ code/web/src/modules/common/AlignCenterMiddle/styles.js
@@ -0,0 +1,14 @@
+// Component Styles
+const styles = {
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    height: '100%',
+    minHeight: 'inherit'
+  }
+}
+
+export default styles
