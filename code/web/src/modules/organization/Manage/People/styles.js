a434765ce273e9cc723541a4bcc295e0f38bfcfd
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-17 10:53:16 +0100

    Organization manage people styles

---
+++ code/web/src/modules/organization/Manage/People/styles.js
@@ -0,0 +1,22 @@
+// Component Styles
+const styles = theme => ({
+  textCenter: {
+    textAlign: 'center'
+  },
+
+  buttonsContainer: {
+    textAlign: 'right',
+    marginTop: theme.spacing()
+  },
+
+  formContainer: {
+    padding: theme.spacing(2)
+  },
+
+  list: {
+    maxHeight: 'calc(100vh - 248px)',
+    overflowY: 'auto'
+  },
+})
+
+export default styles
